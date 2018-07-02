e75a95194220c78132f567c0cd267a44f740bc56
Jose Ortega <jortega928@yahoo.com>
2018-07-02 02:39:08 -0500

    Add project files.

---
+++ GetDnsSrvRecord/GetDnsSrvRecord.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Management.Automation;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace GetDnsSrvRecord
+{
+    [Cmdlet(VerbsCommon.Get, "DNSSrvRecord")]
+    public class GetDnsSrvRecord : Cmdlet
+    {
+
+        [Parameter(Mandatory = true, ValueFromPipeline = true, Position = 1, HelpMessage = "A well formed domain name should be provided")]
+        [ValidatePattern(@"(?=^.{1,253}$)(^(((?!-)[a-zA-Z0-9-]{1,63}(?<!-))|((?!-)[a-zA-Z0-9-]{1,63}(?<!-)\.)+[a-zA-Z]{2,63})$)")]
+        public string Domain { get; set; }
+
+        [Parameter(Mandatory = false, ValueFromPipeline = true, HelpMessage = "SoloSRV is a bool value that tells the script just work on SRV records")]
+        public SwitchParameter SoloSrv { get; set; }
+
+        private StringBuilder OutputSrv { get; set; }
+        private Collection<PSObject> OutputPs { get; set; }
+
+        #region overridefunctions
+
+        protected override void BeginProcessing()
+        {
+            WriteVerbose($"Domain to be queried: {Domain}");
+        }
+        protected override void ProcessRecord()
+        {
+
+            WriteVerbose("Start Processing");
+            if (SoloSrv == true)
+            {
+                OutputSrv = GetSRV(Domain);
+                PSObject returnon = TransformSBintoPso(OutputSrv);
+                WriteObject(returnon);
+            }
+            else
+            {
+                OutputSrv = GetSRV(Domain);
+                OutputPs = GetAllPS(Domain);
+                OutputPs.Add(TransformSBintoPso(OutputSrv));
+                foreach (PSObject psob in OutputPs)
+                {
+                    WriteObject(psob);
+                }
+            }
+            WriteVerbose("End Processing");
+        }
+        protected override void EndProcessing()
+        {
+
+            WriteVerbose("Finished");
+        }
+        protected override void StopProcessing()
+        {
+            WriteObject("Interrupted, exiting now");
+            //base.StopProcessing();
+        }
+        #endregion
+
+        #region Functions
+        StringBuilder GetSRV(string domain)
+        {
+            string command = "/c nslookup -q=srv _autodiscover._tcp." + domain;
+            var process = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "cmd.exe",
+                    Arguments = command,
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    CreateNoWindow = true
+                }
+            };
+            StringBuilder temp = new StringBuilder();
+            process.Start();
+            while (!process.StandardOutput.EndOfStream)
+            {
+                string line = process.StandardOutput.ReadLine();
+                temp.AppendLine(line);
+            }
+            Int16 lines = (Int16)Regex.Matches(temp.ToString(), Environment.NewLine).Count;
+            if (lines <= 4)
+            {
+                temp.AppendLine("There's no DNS SRV record for " + domain);
+            }
+            return temp;
+        }
+        Collection<PSObject> GetAllPS(string domain)
+        {
+            PowerShell powerShellInstance = PowerShell.Create();
+            string script = "Resolve-DnsName " + domain + " -SERVER 8.8.8.8  -Type ALL | select *";
+            powerShellInstance.AddScript(script);
+            return powerShellInstance.Invoke();
+        }
+
+        private PSObject TransformSBintoPso(StringBuilder outputsrv)
+        {
+            string priority = "-";
+            string weight = "-";
+            string port = "-";
+            bool found = false;
+
+            String srvhostname = "-";
+
+
+
+
+            Int16 lines = (Int16)Regex.Matches(outputsrv.ToString(), Environment.NewLine).Count;
+            if (lines > 4)
+            {
+                found = true;
+                Regex regexSrvHost = new Regex(@"[^ = ]+$");
+                Match regexSrv = regexSrvHost.Match(outputsrv.ToString());
+
+                if (regexSrv.Success)
+                {
+                    srvhostname = regexSrv.ToString();
+                }
+
+
+                MatchCollection mc = Regex.Matches(outputsrv.ToString(), @"[?:weight =][0-9]+");
+                int i = 1;
+                foreach (Match match in mc)
+                {
+
+                    foreach (Capture cp in match.Captures)
+                    {
+                        switch (i)
+                        {
+                            case 2:
+                                priority = cp.Value;
+                                break;
+                            case 3:
+                                weight = cp.Value;
+                                break;
+                            case 4:
+                                port = cp.Value;
+                                break;
+                        }
+                        i++;
+                    }
+                }
+            }
+
+            srvhostname = Regex.Replace(srvhostname, @"\t|\n|\r", "");
+            port = Regex.Replace(port, @"\t| ", "");
+            priority = Regex.Replace(priority, @"\t| ", "");
+            port = Regex.Replace(port, @"\t| ", "");
+            weight = Regex.Replace(weight, @"\t| ", "");
+
+            PSObject transformed = new PSObject();
+            transformed.Members.Add(new PSNoteProperty("QueryType", "SVR"));
+            transformed.Members.Add(new PSNoteProperty("Found", found));
+            transformed.Members.Add(new PSNoteProperty("Type", "SVR DNS"));
+            transformed.Members.Add(new PSNoteProperty("Name", "SVR service Location for " + Domain));
+            transformed.Members.Add(new PSNoteProperty("Port", port));
+            transformed.Members.Add(new PSNoteProperty("Priority", priority));
+            transformed.Members.Add(new PSNoteProperty("weight", weight));
+            transformed.Members.Add(new PSNoteProperty("SRV hostname", srvhostname));
+
+            return transformed;
+        }
+        #endregion
+    }
+}
---
+++ TestUnit/Testclass.cs
@@ -0,0 +1,48 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+using GetDnsSrvRecord;
+
+namespace TestUnit
+{
+    [TestFixture]
+    public class Testclass
+    {
+        [Test]
+        public void TestDnsSrvRecord()
+        {
+            var initialSessionState = InitialSessionState.CreateDefault();
+            initialSessionState.Commands.Add(
+                new SessionStateCmdletEntry("Get-DNSSrvRecord", typeof(GetDnsSrvRecord.GetDnsSrvRecord), null)
+                );
+
+            using (var runspace = RunspaceFactory.CreateRunspace(initialSessionState))
+            {
+                runspace.Open();
+                using (var powershell = PowerShell.Create())
+                {
+                    powershell.Runspace = runspace;
+                    var testcommand = new Command("Get-DNSSrvRecord");
+                    testcommand.Parameters.Add("Domain", "j0rt3g4.com");
+
+                    powershell.Commands.AddCommand(testcommand);
+
+                    var results = powershell.Invoke();
+
+
+                    Assert.AreEqual(results.Count(), 4);
+                    Assert.IsNotNull(results[0].Properties["Name"]);
+                    Assert.IsNotNull(results[2].Properties["Name"]);
+                    Assert.IsNotNull(results[1].Properties["Name"]);
+                   
+                }
+            }
+
+        }
+    }
+}
